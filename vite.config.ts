e6abf58581c94c8901ad002d6c30d94d2c2fd293
alfredgrip <alfred.grip@gmail.com>
2025-08-19 14:02:00 +0200

    fix dependency errors

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
+	optimizeDeps: {
+		exclude: ['execa'] // this causes errors in dev server, maybe a future version has fixed it
+	},
 	server: {
 		fs: {
 			allow: ['output']
